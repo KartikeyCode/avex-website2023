58fb0697652159e5f5c77ba7d850bac7178d7ba1
P KARTHIK MANIKANTAN <111214511+Manikantan-S@users.noreply.github.com>
2023-06-18 01:42:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
-import { iconImage1, iconImage2, iconImage3, iconImage4, iconImage5, iconImage6, iconImage11 } from './assets';
 
-
-
+import { iconImage1, iconImage2, iconImage3, iconImage4, iconImage5, iconImage6 } from './assets';
+import TypingEffect from './TypingEffect';
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
         <img src={iconImage4} alt="Icon" className="icon icon4" />
         <img src={iconImage5} alt="Icon" className="icon icon5" />
         <img src={iconImage6} alt="Icon" className="icon icon6" />
-        
         <img
           src="https://imagizer.imageshack.com/img924/5035/Kx4ZJU.png"
           alt="Icon"
@@ -26,33 +24,33 @@ function App() {
           alt="Icon"
           className="icon icon8"
         />
-          <img
+        <img
           src="https://imagizer.imageshack.com/img924/3694/AqnFIq.png"
           alt="Icon"
           className="icon icon9"
         />
-          <img
+        <img
           src="https://imagizer.imageshack.com/img922/3060/HjN0l4.png"
           alt="Icon"
           className="icon icon10"
         />
-          <img
+        <img
           src="https://imagizer.imageshack.com/img924/3403/wq9RqC.jpg"
           alt="Icon"
           className="icon icon11"
         />
-        
-          <img
+
+        <img
           src="https://imagizer.imageshack.com/img923/8944/wzQ0b4.png"
           alt="Icon"
           className="icon icon12"
         />
-           <img
+        <img
           src="https://imagizer.imageshack.com/img923/4822/hbFwto.png"
           alt="Icon"
           className="icon icon13"
         />
-           <img
+        <img
           src="https://imagizer.imageshack.com/img923/9968/MXwTN0.png"
           alt="Icon"
           className="icon icon14"
@@ -62,17 +60,17 @@ function App() {
           alt="Icon"
           className="icon icon15"
         />
-           <img
+        <img
           src="https://imagizer.imageshack.com/img923/1592/XBh9Dj.png"
           alt="Icon"
           className="icon icon16"
         />
-         <img
+        <img
           src="https://imagizer.imageshack.com/img922/6306/XgsQjS.png"
           alt="Icon"
           className="icon icon17"
         />
-          <img
+        <img
           src="https://imagizer.imageshack.com/img923/9083/TBBOaJ.jpg"
           alt="Icon"
           className="icon icon18"
@@ -100,8 +98,23 @@ function App() {
           <li>Contact</li>
         </ul>
       </nav>
+
+      <div className="text-container">
+        <h1 className="text"><center>Your pass to <br />decentralized future</center></h1>
+        <div className="sub-text-container">
+          <h2 className="sub-text">
+            Control your finance,
+            Own your data,
+            Embrace your 
+            <br />community
+          </h2>
+        </div>
+      </div>
+
+      
+      <TypingEffect/>
     </div>
   );
 }
-
 export default App;
+
