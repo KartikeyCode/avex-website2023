6ad468a4c104ad2726ab25fee88b852d7840f463
P KARTHIK MANIKANTAN <111214511+Manikantan-S@users.noreply.github.com>
2023-06-17 11:28:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import iconImage1 from 'file:///C:/Users/Asus/Downloads/avex%20images/USD%20Coin.svg';
-import iconImage2 from 'file:///C:/Users/Asus/Downloads/avex%20images/Group%2046631%20(1).svg';
-import iconImage3 from 'file:///C:/Users/Asus/Downloads/avex%20images/Cube-1%20(3).svg';
-import iconImage4 from 'file:///C:/Users/Asus/Downloads/avex%20images/Color9.svg';
-import iconImage5 from 'file:///C:/Users/Asus/Downloads/Group%2046622%20(1).svg';
+import { iconImage1, iconImage2, iconImage3, iconImage4, iconImage5, iconImage6, iconImage11 } from './assets';
+
+
+
 
 function App() {
   return (
@@ -15,6 +14,84 @@ function App() {
         <img src={iconImage3} alt="Icon" className="icon icon3" />
         <img src={iconImage4} alt="Icon" className="icon icon4" />
         <img src={iconImage5} alt="Icon" className="icon icon5" />
+        <img src={iconImage6} alt="Icon" className="icon icon6" />
+        
+        <img
+          src="https://imagizer.imageshack.com/img924/5035/Kx4ZJU.png"
+          alt="Icon"
+          className="icon icon7"
+        />
+        <img
+          src="https://imagizer.imageshack.com/img924/2272/gnSpqW.png"
+          alt="Icon"
+          className="icon icon8"
+        />
+          <img
+          src="https://imagizer.imageshack.com/img924/3694/AqnFIq.png"
+          alt="Icon"
+          className="icon icon9"
+        />
+          <img
+          src="https://imagizer.imageshack.com/img922/3060/HjN0l4.png"
+          alt="Icon"
+          className="icon icon10"
+        />
+          <img
+          src="https://imagizer.imageshack.com/img924/3403/wq9RqC.jpg"
+          alt="Icon"
+          className="icon icon11"
+        />
+        
+          <img
+          src="https://imagizer.imageshack.com/img923/8944/wzQ0b4.png"
+          alt="Icon"
+          className="icon icon12"
+        />
+           <img
+          src="https://imagizer.imageshack.com/img923/4822/hbFwto.png"
+          alt="Icon"
+          className="icon icon13"
+        />
+           <img
+          src="https://imagizer.imageshack.com/img923/9968/MXwTN0.png"
+          alt="Icon"
+          className="icon icon14"
+        />
+        <img
+          src="https://imagizer.imageshack.com/img922/9087/L6PYHA.png"
+          alt="Icon"
+          className="icon icon15"
+        />
+           <img
+          src="https://imagizer.imageshack.com/img923/1592/XBh9Dj.png"
+          alt="Icon"
+          className="icon icon16"
+        />
+         <img
+          src="https://imagizer.imageshack.com/img922/6306/XgsQjS.png"
+          alt="Icon"
+          className="icon icon17"
+        />
+          <img
+          src="https://imagizer.imageshack.com/img923/9083/TBBOaJ.jpg"
+          alt="Icon"
+          className="icon icon18"
+        />
+        <img
+          src="https://imagizer.imageshack.com/img922/5294/MfIksc.png"
+          alt="Icon"
+          className="icon icon19"
+        />
+        <img
+          src="https://imagizer.imageshack.com/img923/1488/2DE4ea.jpg"
+          alt="Icon"
+          className="icon icon20"
+        />
+        <img
+          src="https://imagizer.imageshack.com/img922/359/CsDern.png"
+          alt="Icon"
+          className="icon icon21"
+        />
         <ul>
           <li>Home</li>
           <li>Services</li>
