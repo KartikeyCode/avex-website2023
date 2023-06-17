85d7a0295f527534bc824db20530f0687a41a951
P KARTHIK MANIKANTAN <111214511+Manikantan-S@users.noreply.github.com>
2023-06-17 11:30:32 +0530

    Create assets.js

---
+++ src/components/assets.js
@@ -0,0 +1,17 @@
+import iconImage1 from 'file:///C:/Users/Asus/Downloads/avex%20images/USD%20Coin.svg';
+import iconImage2 from 'file:///C:/Users/Asus/Downloads/avex%20images/Group%2046631%20(1).svg';
+import iconImage3 from 'file:///C:/Users/Asus/Downloads/avex%20images/Cube-1%20(3).svg';
+import iconImage4 from 'file:///C:/Users/Asus/Downloads/avex%20images/Color9.svg';
+import iconImage5 from 'file:///C:/Users/Asus/Downloads/Group%2046622%20(1).svg';
+import iconImage6 from 'file:///C:/Users/Asus/Downloads/Untitled%20(9)/Sphere/Color9.svg';
+import iconImage7 from 'https://imagizer.imageshack.com/img924/5035/Kx4ZJU.png';
+
+export {
+  iconImage1,
+  iconImage2,
+  iconImage3,
+  iconImage4,
+  iconImage5,
+  iconImage6,
+  iconImage7,
+};
