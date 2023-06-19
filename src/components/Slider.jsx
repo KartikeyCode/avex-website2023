ffd5437cf96b165b3070e007b64c3f56c8049d62
P KARTHIK MANIKANTAN <111214511+Manikantan-S@users.noreply.github.com>
2023-06-19 21:45:20 +0530

    avex

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -11,8 +11,14 @@ const Slider = () => {
     <section className='flex flex-col gap-4'>
       <div className='flex justify-between'>
         <article className='ml-4'>
-          <h1 className="text-4xl font-bold">Slider</h1>
-          <p className="text-center">This is a slider component.</p>
+        <h1 className="text-4xl font-normal leading-58 tracking-normal text-left mb-8">
+       <span className="text-blue-500">Decentralized Identity</span><br/>
+      <span className="text-white">Ecosystem</span>
+      </h1>
+
+      <p className="font-inter text-base font-normal leading-5 tracking-normal text-left mt-8">
+  Manage and own your online identity with self-sovereign identity. Safely<br /> control and shape your personal information giving you freedom and  <br /> choice. Own your digital story and reflect your true self.</p>
+
         </article>
         <CarouselNavigation />
       </div>
