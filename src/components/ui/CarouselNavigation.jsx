d0f0f63dfb2ecb4dd1f0513fffa9034f07373af1
P KARTHIK MANIKANTAN <111214511+Manikantan-S@users.noreply.github.com>
2023-07-01 01:04:09 +0530

    Update CarouselNavigation.jsx

--- src/components/ui/CarouselNavigation.jsx
+++ src/components/ui/CarouselNavigation.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const CarouselNavigation = () => {
   return (
     <div className="flex self-end rounded-full bg-base-200">
-      <a href={`#slide3`} className="btn rounded-full">❮</a> 
-      <a href={`#slide2`} className="btn rounded-full">❯</a>
+      {/* Removed the arrow buttons */}
     </div>
   )
 }
